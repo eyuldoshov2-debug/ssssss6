04fd590fcf5338944b871b3bc84fab996323186e
eyuldoshov2-debug <eyuldoshov2@gmail.com>
2026-01-31 22:16:14 +0500

    Delete scripts directory

--- scripts/002_add_photo_url.sql
+++
@@ -1,2 +0,0 @@
--- Add photo_url column to users table
-ALTER TABLE public.users ADD COLUMN IF NOT EXISTS photo_url text;
--- scripts/005_add_total_spent.sql
+++
@@ -1,2 +0,0 @@
--- Add total_spent column if it doesn't exist
-ALTER TABLE public.users ADD COLUMN IF NOT EXISTS total_spent INTEGER DEFAULT 0;
